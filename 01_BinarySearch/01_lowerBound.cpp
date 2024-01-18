d31a29ac8092c5665a8e50af1b6405e2e5deb069
Mkapoor29 <123291579+Mkapoor29@users.noreply.github.com>
2024-01-18 21:26:17 +0530

    Create 01_lowerBound.cpp

---
+++ 01_BinarySearch/01_lowerBound.cpp
@@ -0,0 +1,2 @@
+// the lowwer bound algorithm finds the first or smallest index in array where value at the index in sorted array is either equal to or more than given target ie x.
+// lower bound is index where arr[index] >= x. if such index doesn't exist, the algorithm returns size of array ie n.
